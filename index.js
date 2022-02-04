7516a73eca771ee94bd05d1a008b09045d0a29aa
sackda75 <dsouks@yahoo.fr>
2022-02-04 22:27:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const colors = require('colors')
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+	const url = req.route.path
+	console.log(`The path is http://localhost:3000${url}`.yellow)
+	res.json({ Backend: 'API Running now', Colors: colors.enabled, Path: `The path is ${url}` })
+})
+
+app.listen(port, () => {
+	console.log(`Running on port ${port} ✅✅`.cyan)
+})
